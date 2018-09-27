6177657712d26e1a38ea8da1f9733c752bfc0b9c
jixingyu <ymx2011@gmail.com>
2018-09-27 23:27:27 +0800

    dist

--- dist/layuiadmin/config.js
+++ dist/layuiadmin/config.js
@@ -6,7 +6,10 @@
  @License：LPPL（layui付费产品协议）
     
  */
- 
+layui.config({
+  version: Date.parse(new Date())
+  ,debug: true
+});
 layui.define(['laytpl', 'layer', 'element', 'util'], function(exports){
   exports('setter', {
     container: 'LAY_app' //容器ID
@@ -43,7 +46,7 @@ layui.define(['laytpl', 'layer', 'element', 'util'], function(exports){
       'echarts', //echarts 核心包
       'echartsTheme' //echarts 主题
     ]
-    
+
     //主题配置
     ,theme: {
       //内置主题配色方案
@@ -101,29 +104,59 @@ layui.define(['laytpl', 'layer', 'element', 'util'], function(exports){
       },{
         header: '#393D49'
         ,alias: 'classic-black-header' //经典黑头
-      },{
-        main: '#50314F'
-        ,logo: '#50314F'
-        ,selected: '#7A4D7B'
-        ,header: '#50314F'
-        ,alias: 'purple-red-header' //紫红头
-      },{
-        main: '#28333E'
-        ,logo: '#28333E'
-        ,selected: '#AA3130'
-        ,header: '#AA3130'
-        ,alias: 'fashion-red-header' //时尚红头
-      },{
-        main: '#28333E'
-        ,logo: '#009688'
-        ,selected: '#009688'
-        ,header: '#009688'
-        ,alias: 'green-header' //墨绿头
       }]
       
       //初始的颜色索引，对应上面的配色方案数组索引
       //如果本地已经有主题色记录，则以本地记录为优先，除非请求本地数据（localStorage）
       ,initColorIndex: 0
     }
+
+    ,api: {
+      GetAreaList: 'http://holtest.fres.cn/PublicMethods/AreaCode/GetAreaList.ashx',
+      GetAreaText: 'http://holtest.fres.cn/PublicMethods/AreaCode/GetAreaText.ashx',
+      SearchICD: 'http://holtest.fres.cn/PublicMethods/ICD10/SearchICD.ashx',
+      GetOftenICD: 'http://holtest.fres.cn/PublicMethods/ICD10/GetOftenICD.ashx',
+      UserLogin: 'http://holtest.fres.cn/User/UserLogin.ashx',
+      UserLogOut: 'http://holtest.fres.cn/User/UserLogOut.ashx',
+      GetUserList: 'http://holtest.fres.cn/User/GetUserList.ashx',
+      GetUserInfo: 'http://holtest.fres.cn/User/GetUserInfo.ashx',
+      ModifyUserInfo: 'http://holtest.fres.cn/User/ModifyUserInfo.ashx',
+      GetLoginUser: 'http://holtest.fres.cn/User/GetLoginUser.ashx',
+      DeleteUser: 'http://holtest.fres.cn/User/DeleteUser.ashx',
+      ChangeUserPassword: 'http://holtest.fres.cn/User/ChangeUserPassword.ashx',
+      GetConfigDetail: 'http://holtest.fres.cn/Config/GetConfigDetail.ashx',
+      GetConfigInfo: 'http://holtest.fres.cn/Config/GetConfigInfo.ashx',
+      GetHospitalUnit: 'http://holtest.fres.cn/Hospital/GetHospitalUnit.ashx',
+      ModificationHospitalUnit: 'http://holtest.fres.cn/Hospital/ModificationHospitalUnit.ashx',
+      DeleteHospitalUnit: 'http://holtest.fres.cn/Hospital/DeleteHospitalUnit.ashx',
+      GetDataFormClientID: 'http://holtest.fres.cn/Equipment/GetDataFormClientID.ashx',
+      GetEquipmentType: 'http://holtest.fres.cn/Equipment/GetEquipmentType.ashx',
+      GetEquipmentList: 'http://holtest.fres.cn/Equipment/GetEquipmentList.ashx',
+      GetEquipmentInfo: 'http://holtest.fres.cn/Equipment/GetEquipmentInfo.ashx',
+      RegisterEquipment: 'http://holtest.fres.cn/Equipment/RegisterEquipment.ashx',
+      ModifyEquipment: 'http://holtest.fres.cn/Equipment/ModifyEquipment.ashx',
+      DeleteEquipment: 'http://holtest.fres.cn/Equipment/DeleteEquipment.ashx',
+      GetEquipmentData: 'http://holtest.fres.cn/Equipment/GetEquipmentData.ashx',
+      ModifyClientInfo: 'http://holtest.fres.cn/Client/ModifyClientInfo.ashx',
+      GetClientInfo: 'http://holtest.fres.cn/Client/GetClientInfo.ashx',
+      SearchClient: 'http://holtest.fres.cn/Client/SearchClient.ashx',
+      DeleteClient: 'http://holtest.fres.cn/Client/DeleteClient.ashx',
+      UpFile: 'http://holtest.fres.cn/PublicMethods/UpLoad/UpFile.ashx',
+      ReadFile: 'http://holtest.fres.cn/PublicMethods/UpLoad/ReadFile.ashx',
+      ModifyClientHistory: 'http://holtest.fres.cn/Client/History/ModifyClientHistory.ashx',
+      DeleteClientHistory: 'http://holtest.fres.cn/Client/History/DeleteClientHistory.ashx',
+      GetClientHistory: 'http://holtest.fres.cn/Client/History/GetClientHistory.ashx',
+      GetPhysicalExaminationList: 'http://holtest.fres.cn/PhysicalExamination/GetPhysicalExaminationList.ashx',
+      GetPhysicalExaminationInfo: 'http://holtest.fres.cn/PhysicalExamination/GetPhysicalExaminationInfo.ashx',
+      NewPhysicalExamination: 'http://holtest.fres.cn/PhysicalExamination/NewPhysicalExamination.ashx',
+      DeletePhysicalExamination: 'http://holtest.fres.cn/PhysicalExamination/DeletePhysicalExamination.ashx',
+      SavePhysicalExaminationData: 'http://holtest.fres.cn/PhysicalExamination/SavePhysicalExaminationData.ashx',
+    }
+    ,loadsec: 5*1000
+    ,dateFormat: {
+      day: 'yyyy/MM/dd'
+      ,sec: 'yyyy/MM/dd HH:mm:ss'
+    }
+    ,baseUrl: '/src/views/'
   });
 });

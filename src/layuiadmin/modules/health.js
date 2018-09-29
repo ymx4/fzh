46fde38c05f4b717a6e2c5c10471d2a70c8fe442
jixingyu <ymx2011@gmail.com>
2018-09-29 16:39:27 +0800

    update

--- src/layuiadmin/modules/health.js
+++ src/layuiadmin/modules/health.js
@@ -31,24 +31,30 @@ layui.define(['table', 'form', 'laytpl', 'common', 'history'], function(exports)
         } else {
           item.Physical_Detail.INPUT_OTHER_VALUE = '';
         }
-        var html = laytpl('<label class="layui-form-label"{{#  if(d.Physical_Detail.longTitle){ }} style="width:220px;"{{#  } }}>\
-          {{#  if(d.Physical_Detail.REMARK && d.Physical_Detail.REMARK != ""){ }}\
+        var html = laytpl('<label class="layui-form-label"{{# if(d.Physical_Detail.longTitle){ }} style="width:220px;"{{# } }}>\
+          {{# if(d.Physical_Detail.REMARK && d.Physical_Detail.REMARK != ""){ }}\
             <i class="layui-icon layui-icon-tips" lay-tips="{{d.Physical_Detail.REMARK}}"></i> \
-          {{#  } }}{{d.Physical_Detail.CHINESE_NAME}}</label>\
+          {{# } }}{{d.Physical_Detail.CHINESE_NAME}}</label>\
           <div class="layui-input-block">\
-            <input type="radio" name="hproject_{{d.Physical_Detail.PROJECT_ID}}"{{#  if(d.Physical_Detail.STATUS == 0){ }} checked{{#  } }} value="未检" title="未检" lay-filter="xy-form-extra-input">\
-            {{#  layui.each(d.Project_ENUM_DATA_List, function(index, tplitem){ }}\
-              {{#  if(tplitem.DATA_VALUE == "OTHER_VALUE"){ }}\
-                <input type="radio" name="hproject_{{d.Physical_Detail.PROJECT_ID}}"{{#  if(d.Physical_Detail.INPUT_OTHER_VALUE != ""){ }} checked{{#  } }} value="{{tplitem.DATA_VALUE}}" title="{{tplitem.ENUM_VALUE}}" data-show="1" lay-filter="xy-form-extra-input">\
-                <div class="xy-form-extra-control extra-control-other{{#  if(d.Physical_Detail.INPUT_OTHER_VALUE == ""){ }} layui-hide{{#  } }}">\
-                  <input type="text" name="other_hproject_{{d.Physical_Detail.PROJECT_ID}}" value="{{d.Physical_Detail.INPUT_OTHER_VALUE}}" autocomplete="off" class="layui-input xy-form-unit">\
-                </div>\
-              {{#  } else if(d.Physical_Detail.hasOther) { }}\
-                <input type="radio" name="hproject_{{d.Physical_Detail.PROJECT_ID}}"{{#  if (d.Physical_Detail.DATA_VALUE == tplitem.DATA_VALUE) { }} checked{{#  } }} value="{{tplitem.DATA_VALUE}}" title="{{tplitem.ENUM_VALUE}}" lay-filter="xy-form-extra-input">\
-              {{#  } else { }}\
-                <input type="radio" name="hproject_{{d.Physical_Detail.PROJECT_ID}}"{{#  if (d.Physical_Detail.DATA_VALUE == tplitem.DATA_VALUE) { }} checked{{#  } }} value="{{tplitem.DATA_VALUE}}" title="{{tplitem.ENUM_VALUE}}">\
-              {{#  } }}\
-            {{#  }); }}\
+            <input type="radio" name="hproject_{{d.Physical_Detail.PROJECT_ID}}"{{# if(d.Physical_Detail.STATUS == 0){ }} checked{{# } }} value="未检" title="未检" lay-filter="xy-form-extra-input">\
+            {{# layui.each(d.Project_ENUM_DATA_List, function(index, tplitem){ }}\
+              {{# if(tplitem.DATA_VALUE == "OTHER_VALUE"){ }}\
+                <input type="radio" name="hproject_{{d.Physical_Detail.PROJECT_ID}}"{{# if(d.Physical_Detail.INPUT_OTHER_VALUE != ""){ }} checked{{# } }} value="{{tplitem.DATA_VALUE}}" title="{{tplitem.ENUM_VALUE}}" data-show="1" lay-filter="xy-form-extra-input">\
+                {{# if(d.Physical_Detail.PROJECT_ID == 121){ }}\
+                  <div class="xy-form-extra-control extra-control-link xy-form-extra-l{{# if(d.Physical_Detail.INPUT_OTHER_VALUE == ""){ }} layui-hide{{# } }}">\
+                    <textarea name="other_hproject_{{d.Physical_Detail.PROJECT_ID}}" autocomplete="off" rows=1 class="layui-input">{{d.Physical_Detail.INPUT_OTHER_VALUE}}</textarea>\
+                  </div>\
+                {{# } else { }}\
+                  <div class="xy-form-extra-control extra-control-link{{# if(d.Physical_Detail.INPUT_OTHER_VALUE == ""){ }} layui-hide{{# } }}">\
+                    <input type="text" name="other_hproject_{{d.Physical_Detail.PROJECT_ID}}" value="{{d.Physical_Detail.INPUT_OTHER_VALUE}}" autocomplete="off" class="layui-input">\
+                  </div>\
+                {{# } }}\
+              {{# } else if(d.Physical_Detail.hasOther) { }}\
+                <input type="radio" name="hproject_{{d.Physical_Detail.PROJECT_ID}}"{{# if (d.Physical_Detail.DATA_VALUE == tplitem.DATA_VALUE) { }} checked{{# } }} value="{{tplitem.DATA_VALUE}}" title="{{tplitem.ENUM_VALUE}}" lay-filter="xy-form-extra-input">\
+              {{# } else { }}\
+                <input type="radio" name="hproject_{{d.Physical_Detail.PROJECT_ID}}"{{# if (d.Physical_Detail.DATA_VALUE == tplitem.DATA_VALUE) { }} checked{{# } }} value="{{tplitem.DATA_VALUE}}" title="{{tplitem.ENUM_VALUE}}">\
+              {{# } }}\
+            {{# }); }}\
           </div>').render(item);
         $('#hproject_' + item.Physical_Detail.PROJECT_ID).html(html);
         break;
@@ -74,19 +80,25 @@ layui.define(['table', 'form', 'laytpl', 'common', 'history'], function(exports)
         }
         var html = laytpl('<label class="layui-form-label">{{d.Physical_Detail.CHINESE_NAME}}</label>\
           <div class="layui-input-block">\
-            {{#  if(d.Physical_Detail.hasDefault){ }}\
-            <input type="checkbox" name="hproject_{{d.Physical_Detail.PROJECT_ID}}" lay-skin="primary"{{#  if(d.Physical_Detail.STATUS == 0){ }} checked{{#  } }} value="未检" title="未检" lay-filter="xy-form-extra-input">\
-            {{#  } }}\
-            {{#  layui.each(d.Project_ENUM_DATA_List, function(index, tplitem){ }}\
-              {{#  if(tplitem.DATA_VALUE == "OTHER_VALUE"){ }}\
-                <input type="checkbox" name="hproject_{{d.Physical_Detail.PROJECT_ID}}" lay-skin="primary"{{#  if(d.Physical_Detail.INPUT_OTHER_VALUE != ""){ }} checked{{#  } }} value="{{tplitem.DATA_VALUE}}" title="{{tplitem.ENUM_VALUE}}" lay-filter="xy-form-extra-input">\
-                <div class="xy-form-extra-control extra-control-other{{#  if(d.Physical_Detail.INPUT_OTHER_VALUE == ""){ }} layui-hide{{#  } }}">\
-                  <input type="text" name="other_hproject_{{d.Physical_Detail.PROJECT_ID}}" value="{{d.Physical_Detail.INPUT_OTHER_VALUE}}" autocomplete="off" class="layui-input xy-form-unit">\
-                </div>\
-              {{#  } else { }}\
-                <input type="checkbox" name="hproject_{{d.Physical_Detail.PROJECT_ID}}" lay-skin="primary"{{#  if (d.Physical_Detail.DATA_VALUE.indexOf(tplitem.DATA_VALUE) != -1) { }} checked{{#  } }} value="{{tplitem.DATA_VALUE}}" title="{{tplitem.ENUM_VALUE}}" lay-filter="xy-form-extra-check">\
-              {{#  } }}\
-            {{#  }); }}\
+            {{# if(d.Physical_Detail.hasDefault){ }}\
+            <input type="checkbox" name="hproject_{{d.Physical_Detail.PROJECT_ID}}" lay-skin="primary"{{# if(d.Physical_Detail.STATUS == 0){ }} checked{{# } }} value="未检" title="未检" lay-filter="xy-form-extra-input">\
+            {{# } }}\
+            {{# layui.each(d.Project_ENUM_DATA_List, function(index, tplitem){ }}\
+              {{# if(tplitem.DATA_VALUE == "OTHER_VALUE"){ }}\
+                <input type="checkbox" name="hproject_{{d.Physical_Detail.PROJECT_ID}}" lay-skin="primary"{{# if(d.Physical_Detail.INPUT_OTHER_VALUE != ""){ }} checked{{# } }} value="{{tplitem.DATA_VALUE}}" title="{{tplitem.ENUM_VALUE}}" lay-filter="xy-form-extra-input">\
+                {{# if(d.Physical_Detail.PROJECT_ID == 123){ }}\
+                  <div class="xy-form-extra-control extra-control-link xy-form-extra-l{{# if(d.Physical_Detail.INPUT_OTHER_VALUE == ""){ }} layui-hide{{# } }}">\
+                    <textarea name="other_hproject_{{d.Physical_Detail.PROJECT_ID}}" autocomplete="off" rows=1 class="layui-input">{{d.Physical_Detail.INPUT_OTHER_VALUE}}</textarea>\
+                  </div>\
+                {{# } else { }}\
+                  <div class="xy-form-extra-control extra-control-link{{# if(d.Physical_Detail.INPUT_OTHER_VALUE == ""){ }} layui-hide{{# } }}">\
+                    <input type="text" name="other_hproject_{{d.Physical_Detail.PROJECT_ID}}" value="{{d.Physical_Detail.INPUT_OTHER_VALUE}}" autocomplete="off" class="layui-input">\
+                  </div>\
+                {{# } }}\
+              {{# } else { }}\
+                <input type="checkbox" name="hproject_{{d.Physical_Detail.PROJECT_ID}}" lay-skin="primary"{{# if (d.Physical_Detail.DATA_VALUE.indexOf(tplitem.DATA_VALUE) != -1) { }} checked{{# } }} value="{{tplitem.DATA_VALUE}}" title="{{tplitem.ENUM_VALUE}}" lay-filter="xy-form-extra-check">\
+              {{# } }}\
+            {{# }); }}\
           </div>').render(item);
         $('#hproject_' + item.Physical_Detail.PROJECT_ID).html(html);
         break;
@@ -330,19 +342,27 @@ layui.define(['table', 'form', 'laytpl', 'common', 'history'], function(exports)
             });
 
             $('#bmiDiv').text(inputData['hproject_25'] ? inputData['hproject_25'] : '');
-            $('#hproject_29 .layui-input-block').append('<div class="xy-form-extra-control">\
-                <div class="layui-form-mid">简易智力状态检查，总分</div>\
-                <input type="text" name="hproject_129" autocomplete="off" class="layui-input xy-form-unit" style="width: 100px;">\
+            $('#hproject_29 :radio').attr('lay-filter', 'xy-form-extra-input');
+            $('#hproject_29 :radio[title="粗筛阳性"]').attr('data-show', 1);
+            $('#hproject_29 .layui-input-block').append('<div class="xy-form-extra-control extra-control-link' + (common.empty(inputData['hproject_129']) ? ' layui-hide' : '') + '">\
+              <div class="layui-form-mid">简易智力状态检查，总分</div>\
+                <input type="text" name="hproject_129" autocomplete="off" class="layui-input" style="width: 100px;">\
               </div>');
-            $('#hproject_30 .layui-input-block').append('<div class="xy-form-extra-control">\
+            $('#hproject_30 :radio').attr('lay-filter', 'xy-form-extra-input');
+            $('#hproject_30 :radio[title="粗筛阳性"]').attr('data-show', 1);
+            $('#hproject_30 .layui-input-block').append('<div class="xy-form-extra-control extra-control-link' + (common.empty(inputData['hproject_128']) ? ' layui-hide' : '') + '">\
                 <div class="layui-form-mid">老年人抑郁评分检查，总分</div>\
-                <input type="text" name="hproject_128" autocomplete="off" class="layui-input xy-form-unit" style="width: 100px;">\
+                <input type="text" name="hproject_128" autocomplete="off" class="layui-input" style="width: 100px;">\
               </div>');
-            $('#hproject_41 .layui-input-block').find(':radio[title="已戒酒"]').after('<div class="xy-form-extra-control">\
+            $('#hproject_41 :radio').attr('lay-filter', 'xy-form-extra-input');
+            $('#hproject_41 :radio[title="已戒酒"]').attr('data-show', 1);
+            $('#hproject_41 .layui-input-block').find(':radio[title="已戒酒"]').after('<div class="xy-form-extra-control extra-control-link' + (common.empty(inputData['hproject_130']) ? ' layui-hide' : '') + '">\
                 <div class="layui-form-mid">戒酒年龄</div>\
-                <input type="text" name="hproject_130" autocomplete="off" class="layui-input xy-form-unit" style="width: 100px;">\
+                <input type="text" name="hproject_130" autocomplete="off" class="layui-input" style="width: 100px;">\
               </div>');
-            $('#hproject_46 .layui-input-block').find(':radio[title="有"]').after('<div class="xy-form-extra-control">\
+            $('#hproject_46 :radio').attr('lay-filter', 'xy-form-extra-input');
+            $('#hproject_46 :radio[title="有"]').attr('data-show', 1);
+            $('#hproject_46 .layui-input-block').find(':radio[title="有"]').after('<div class="xy-form-extra-control extra-control-link' + ((common.empty(inputData['hproject_131']) && common.empty(inputData['hproject_132'])) ? ' layui-hide' : '') + '">\
                 <div class="layui-form-mid">工种</div>\
                 <input type="text" name="hproject_131" autocomplete="off" class="layui-input xy-form-unit" style="width: 100px;">\
                 <div class="layui-form-mid">从业时间</div>\
@@ -471,8 +491,8 @@ layui.define(['table', 'form', 'laytpl', 'common', 'history'], function(exports)
           if (data.elem.checked) {
             curElem.nextAll(':checkbox').prop("checked", false);
             form.render('checkbox');
-            curElem.nextAll('.extra-control-other').addClass('layui-hide');
-            curElem.nextAll('.xy-form-extra-control').find('input').val('');
+            curElem.nextAll('.extra-control-link').addClass('layui-hide');
+            curElem.nextAll('.xy-form-extra-control').find('input,textarea').val('');
           }
         } else {
           if (data.elem.checked) {
@@ -482,7 +502,7 @@ layui.define(['table', 'form', 'laytpl', 'common', 'history'], function(exports)
             }
             curElem.nextAll('.xy-form-extra-control:first').removeClass('layui-hide');
           } else {
-            curElem.nextAll('.xy-form-extra-control:first').addClass('layui-hide').find('input').val('');
+            curElem.nextAll('.xy-form-extra-control:first').addClass('layui-hide').find('input,textarea').val('');
           }
         }
       });
@@ -499,7 +519,7 @@ layui.define(['table', 'form', 'laytpl', 'common', 'history'], function(exports)
         if ($(data.elem).data('show') == 1) {
           curElem.nextAll('.xy-form-extra-control:first').removeClass('layui-hide');
         } else {
-          curElem.nextAll('.xy-form-extra-control:first').addClass('layui-hide').find('input').val('');
+          curElem.nextAll('.xy-form-extra-control:first').addClass('layui-hide').find('input,textarea').val('');
         }
       });
     }

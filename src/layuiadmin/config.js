d58581ef1e7faa9b750e4130ec18b84534806690
jixingyu <ymx2011@gmail.com>
2018-10-23 22:18:08 +0800

    update

--- src/layuiadmin/config.js
+++ src/layuiadmin/config.js
@@ -176,6 +176,6 @@ layui.define(['laytpl', 'layer', 'element', 'util'], function(exports){
       day: 'yyyy/MM/dd'
       ,sec: 'yyyy/MM/dd HH:mm:ss'
     }
-    ,baseUrl: '/src/views/'
+    ,baseUrl: '/html/views/'
   });
 });
